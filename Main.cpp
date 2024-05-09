6b6afb305c6f965a7eb9a3a19984629a387e20eb
socialcredit64 <124119894+socialcredit64@users.noreply.github.com>
2024-05-09 09:16:24 -0500

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -5,38 +5,14 @@
 
 
 int main() {
-   std::cout << "Hello World!";
+   std::cout << "Printing Contents";
    // Open the file for reading
-   std::fstream fio;
- 
    std::string line;
-
-
-   fio.open("sample.txt", ios::trunc | ios::out | ios::in);
-
-   // Execute a loop If file successfully Opened
-   while (fio) {
- 
-      // Read a Line from standard input
-      std::getline(cin, line);
-
-      // Press -1 to exit
-      if (line == "-1")
-         break;
-
-      // Write line in file
-      fio << line << std::endl;
+   std::ifstream ReadFile("example.txt");
+   
+   while (std::getline (ReadFile, line)){
+      std::cout << line << "\n";
    }
-
-
-   fio.seekg(0, std::ios::beg);
-
-   while (fio) {
-      std::getline(fio, line);
-      std::cout << line << std::endl;
-   }
-
-   fio.close();
- 
+   
    return 0;
 }
